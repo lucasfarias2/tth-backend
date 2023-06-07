2186f2818cd869376415e3c3794a6c42b86fa562
Lucas Farias <lukf95@gmail.com>
2023-06-07 19:19:00 +0200

    Add objective and goals model

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Pack
+from .models import Task, Objective, Goal
 
-admin.site.register(Pack)
+admin.site.register(Task)
+
+admin.site.register(Objective)
+
+admin.site.register(Goal)
--- api/models.py
+++ api/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-class Pack(models.Model):
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Objective(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Goal(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
