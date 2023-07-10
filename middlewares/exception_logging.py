4e28b85b9a36c71a00d65ca06675a3a7f76906b8
Lucas Farias <lukf95@gmail.com>
2023-07-10 21:18:13 +0200

    Add mw

---
+++ middlewares/exception_logging.py
@@ -0,0 +1,13 @@
+import traceback
+import logging
+
+class ExceptionLoggingMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        logging.error(traceback.format_exc())  # logs the exception
